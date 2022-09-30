c008c0c6f925c87f870e28328aeeca5c78b4dafb
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-09-30 12:21:51 +0300

    Completed challenge

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
 function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+  const [firstName, setFirstName] = useState("Dismas");
+  const [lastName, setLastName] = useState("Ngoda");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +13,47 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  const [errors, setErrors] = useState([]);
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    // Checking mandatory field, firstName
+    if(firstName.length>0){
+    const formData = { firstName: firstName, lastName: lastName};
+    const dataArray = {...submittedData, formData};
+    setSubmittedData(dataArray);
+    // props.sendFormDataSomewhere(formData);
+    setFirstName("");
+    setLastName("");
+    setErrors([]);
+    }
+    else{
+      setErrors(["First name and last name must be field!"]);
+    }
+
+  }
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
   return (
-    <form>
+    <div>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    {/* Rendering error message to the user */}
+    {errors.length > 0 ? errors.map((error, index) =>(
+      <p key={index} style={{color:"red"}}>{error}</p>
+    )) : null}
+
+    <h3>Submitted Data</h3>
+    {listOfSubmissions}
+    </div>
   );
 }
 
